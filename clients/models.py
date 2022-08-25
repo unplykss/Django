ae7b30ed78d0d6276c699f62eb82e95160bbbc6f
polopeepka <109268514+unplykss@users.noreply.github.com>
2022-08-25 14:07:57 +0600

    Update models.py

--- clients/models.py
+++ clients/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Client(models.Model):
-    name = models.CharField(max_length=250)
-    address = models.CharField(max_length=250)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     active = models.BooleanField(default=True)
-    bottles_ordered =  models.IntegerField(default=1)
+    bottles_ordered = models.IntegerField(default=1)
+    photo = models.ImageField(
+        verbose_name="Фото",
+        upload_to='media/photos',
+        null=True,
+        blank=True
+    )
